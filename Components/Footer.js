a8e8ad620d9fc492ebd97b1a940375e3d805b09f
Kenneth Holt <kingholt@gmail.com>
2023-01-11 00:13:40 -0800

    added files

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 class Footer extends React.Component{
   render(){
-    return <p>Ken Holt</p>
+    return <p>Author: Ken Holt</p>
   }
 }
 
---
+++ Components/HornedBeast.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { CarouselItem } from 'react-bootstrap';
+
+
+
+class HornedBeast extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      likes: 0,
+    }
+  }
+
+  handleLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  }
+
+
+  render() {
+    return (
+      <>
+        <article>
+          <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>{this.props.title}</Card.Title>
+              <Card.Text>{this.state.likes}Likes</Card.Text>
+            </Card.Body>
+          </Card>
+        </article>
+      </>
+    )
+  }
+
+}
+
+export default HornedBeast;
+
+  // render() {
+    // return (
+      // <>
+      {/* <article> */}
+        {/* <h2>{this.props.title}</h2> */}
+{/*  */}
+        {/* <img src={this.props.image}  */}
+          // alt={this.description} 
+          // title={this.props.title} />
+{/*  */}
+        {/* <p>{this.props.description}</p> */}
+      {/* </article> */}
+      {/* </> */}
+    // )
+  // }
+
+
